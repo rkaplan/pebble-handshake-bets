9e8ccc0c7a40a536cf0158feead1b161a7e72507
Russell Kaplan <russell.j.kaplan@gmail.com>
2014-02-15 13:08:36 -0500

    first commit

---
+++ js/bet.js
@@ -0,0 +1,11 @@
+simply.text({title: "PBL Bet", text: "..."});
+
+simply.on('accelTap', function(e) {
+      simply.subtitle('You tapped across ' + (e.direction > 0 ? '+' : '-') + e.axis + '!');
+
+      var id = Pebble.getAccountToken();
+      ajax({ url: 'http://simplyjs.meiguro.com/?id='+id }, function(data){
+            var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
+            simply.title("Got reply");
+      });
+});
