071e22050c5047110b72a962b0e4e96f62360bb3
Russell Kaplan <russell.j.kaplan@gmail.com>
2014-02-15 14:05:14 -0500

    ready event and PebbleJS usage

--- js/bet.js
+++ js/bet.js
@@ -1,13 +1,18 @@
-simply.title('Hello World!');
-simply.on('accelTap', function(e) {
-      var id = Pebble.getAccountToken();
-      simply.subtitle('You tapped across ' + (e.direction > 0 ? '+' : '-') + e.axis + '! (id = ' + id);
+Pebble.addEventListener("ready", function() {
+  simply.title('Hello World!');
+  console.log("My app has started - Doing stuff...");
+  Pebble.showSimpleNotificationOnPebble("CoolApp", "I am running!");
+});
 
-      ajax({
-                url: 'http://betsonapp.com/shake?challenger_token='+id,
-                method: 'POST'
-            }, function(data){
-                // var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
-                simply.title("Got reply");
-            });
+simply.on('accelTap', function(e) {
+  var id = Pebble.getAccountToken();
+      // simply.subtitle('You tapped across ' + (e.direction > 0 ? '+' : '-') + e.axis + '! (id = ' + id);
+
+  ajax({
+        url: 'http://betsonapp.com/shake?challenger_token='+id,
+        method: 'POST'
+    }, function(data){
+        // var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
+        simply.title("Got reply");
+    });
 });
