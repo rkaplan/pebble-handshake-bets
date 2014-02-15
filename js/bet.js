185f4816e71f30cb8c6d9c933f660c8c8c5a5d74
Russell Kaplan <russell.j.kaplan@gmail.com>
2014-02-15 13:24:00 -0500

    testing accelTap

--- js/bet.js
+++ js/bet.js
@@ -1,12 +1,12 @@
 simply.title('Hello World!');
 
 
-// simply.on('accelTap', function(e) {
-//       simply.subtitle('You tapped across ' + (e.direction > 0 ? '+' : '-') + e.axis + '!');
+simply.on('accelTap', function(e) {
+      var id = Pebble.getAccountToken();
+      simply.subtitle('You tapped across ' + (e.direction > 0 ? '+' : '-') + e.axis + '! (id = ' + id);
 
-//       var id = Pebble.getAccountToken();
-//       ajax({ url: 'http://simplyjs.meiguro.com/?id='+id }, function(data){
-//             var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
-//             simply.title("Got reply");
-//       });
-// });
+      // ajax({ url: 'http://simplyjs.meiguro.com/?id='+id }, function(data){
+      //       var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
+      //       simply.title("Got reply");
+      // });
+});
