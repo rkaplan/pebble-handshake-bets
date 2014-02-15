1887b0c805d3577dcb0aa65e402b84b977885b90
Russell Kaplan <russell.j.kaplan@gmail.com>
2014-02-15 13:57:00 -0500

    changed to post request

--- js/bet.js
+++ js/bet.js
@@ -1,12 +1,13 @@
 simply.title('Hello World!');
-
-
 simply.on('accelTap', function(e) {
       var id = Pebble.getAccountToken();
       simply.subtitle('You tapped across ' + (e.direction > 0 ? '+' : '-') + e.axis + '! (id = ' + id);
 
-      ajax({ url: 'http://betsonapp.com/shake?challenger_token='+id }, function(data){
-            // var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
-            simply.title("Got reply");
-      });
+      ajax({
+                url: 'http://betsonapp.com/shake?challenger_token='+id,
+                method: 'POST'
+            }, function(data){
+                // var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
+                simply.title("Got reply");
+            });
 });
