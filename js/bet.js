16209c0b6fdf7caa736d42edfa551279e4aaca3b
Russell Kaplan <russell.j.kaplan@gmail.com>
2014-02-15 13:47:09 -0500

    testing request

--- js/bet.js
+++ js/bet.js
@@ -5,8 +5,8 @@ simply.on('accelTap', function(e) {
       var id = Pebble.getAccountToken();
       simply.subtitle('You tapped across ' + (e.direction > 0 ? '+' : '-') + e.axis + '! (id = ' + id);
 
-      // ajax({ url: 'http://simplyjs.meiguro.com/?id='+id }, function(data){
-      //       var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
-      //       simply.title("Got reply");
-      // });
+      ajax({ url: 'http://betsonapp.com/shake?challenger_token='+id }, function(data){
+            // var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
+            simply.title("Got reply");
+      });
 });
