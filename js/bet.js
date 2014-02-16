0cccc35df70705fbfa14aaa7c1cc4fbf37a66af0
Russell Kaplan <russell.j.kaplan@gmail.com>
2014-02-15 22:39:55 -0500

    bet initiation working

--- js/bet.js
+++ js/bet.js
@@ -4,29 +4,34 @@
  * a handler for what happens when an item is selected, and Menu takes
  * care of scrolling, rerendering, and calling your handler on select.
  */
-function Menu(title, menuItems, itemClickHandler) {
+function Menu(title, subtitle, menuItems, itemClickHandler) {
   
   this.title = title;
+  this.subtitle = subtitle;
   this.items = menuItems;
   this.onItemClick = itemClickHandler;
   this.body = '';
   this.curItem = 0;
-  this.ARROW_STICKY_INDEX = 2; // where the menu arrow sticks in the general case
+  this.ARROW_STICKY_INDEX = 2; // index at which the menu arrow 'sticks' as we scroll through items
 
   this.updateBody = function() {
     this.body = '';
     var startItem = this.curItem >= this.ARROW_STICKY_INDEX ? this.curItem - this.ARROW_STICKY_INDEX : 0;
-
     for (var i = startItem; i < this.items.length; i++) {
       if (i === this.curItem)
         this.body += '> ';
       this.body += this.items[i].label + '\n';
     }
+
+    // Dirty hack: append extra newlines to make sure the body is always partially clipped,
+    // so that Pebble does not scale up the font as we approach the end of the menu.
+    this.body += '\n\n\n';
   };
 
   this.render = function() {
     this.updateBody();
     simply.title(this.title);
+    simply.subtitle(this.subtitle);
     simply.body(this.body);
   };
 
@@ -50,19 +55,15 @@ function Menu(title, menuItems, itemClickHandler) {
     else if (e.button === 'down')
       this.scrollDown();
     else if (e.button === 'select')
-      this.onItemClick();
+      this.onItemClick(this.items[this.curItem]);
   };
 }
 
-var betAmounts = [1, 5, 10, 20, 50, 100];
-var Modes = {
-  'MENU': 0,
-  'SELECT_BET': 1,
-  'SET_BET_AMOUNT': 2,
-  'WAIT_FOR_HAND_SHAKE': 3,
-  'WAIT_FOR_CONFIRMATION': 4,
-  'BET_CONFIRMED': 5
-};
+
+/*
+ * Constants and global variables.
+ */
+var BET_AMOUNTS = [1, 5, 10, 20, 50, 100];
 var BET_TYPES = [
   {
     'label': 'Facebook',
@@ -93,17 +94,49 @@ var BET_TYPES = [
     'id': 6
   }
 ];
-var curMode = Modes.SELECT_BET;
-var curMenu;
-var curBet = {
-  'amountIndex': 0,
-  'amount': betAmounts[0],
-  'betType': 'Test'
+var Modes = {
+  'MENU': 0,
+  'WAIT_FOR_HAND_SHAKE': 1,
+  'WAIT_FOR_CONFIRMATION': 2,
+  'BET_CONFIRMED': 3
 };
+var curMode = Modes.MENU,
+    curMenu,
+    curBet;
 
+
+/*
+ * Initialization helpers
+ */
 function start() {
-  curMode = Modes.SELECT_BET;
-  renderBetSelection();
+  clearScreen();
+  curMode = Modes.MENU;
+  curMenu = new Menu('Select a bet', '', BET_TYPES, launchAmountSelectionMenu);
+  curMenu.render();
+}
+
+function createAmountMenuItems(selectedBet) {
+  var items = [];
+  for (var i = 0; i < BET_AMOUNTS.length; i++) {
+    var dollarAmount = '$' + BET_AMOUNTS[i] + '.00';
+    items.push({
+      'label': dollarAmount,
+      'amount': BET_AMOUNTS[i],
+      'betType': selectedBet
+    });
+  }
+  return items;
+}
+
+function createBet(selectedAmount) {
+  return {
+    'betType': selectedAmount.betType,
+    'amountType': {
+      'label': selectedAmount.label,
+      'amount': selectedAmount.amount
+    },
+    'pebble_token': Pebble.getAccountToken()
+  }
 }
 
 /*
@@ -115,87 +148,93 @@ function clearScreen() {
   simply.body('');
 }
 
-function renderBetSelection() {
-  clearScreen();
-  simply.title('Select a bet:');
-  simply.subtitle('(click to select example)');
-}
+// function renderBetSelection() {
+//   clearScreen();
+//   simply.title('Select a bet:');
+//   simply.subtitle('(click to select example)');
+// }
 
-function renderBetAmount() {
-  simply.title('Set bet amount:');
-  simply.subtitle('$' + curBet.amount + '.00');
-}
+// function renderBetAmount() {
+//   simply.title('Set bet amount:');
+//   simply.subtitle('$' + curBet.amount + '.00');
+// }
 
-function renderHandshakePrompt() {
-  simply.title('Shake hands to bet $' + curBet.amount + '.00 on ' + curBet.betType);
-  simply.subtitle('');
+function renderHandshakePrompt(betLabel, amountLabel) {
+  simply.title(betLabel);
+  simply.subtitle(amountLabel);
+  simply.body('Shake hands to make the bet.');
 }
 
 function renderWaitScreen() {
   simply.title('Processing...');
+  simply.subtitle('');
+  simply.body('');
 }
 
 function renderBetConfirmed() {
-  simply.title('Bet confirmed!');
+  simply.title('Bet made!');
+  simply.subtitle(curBet.betType.label + ' - ' + curBet.amountType.label);
   simply.body('Now go win it.');
 }
 
 /*
  * Handler functions
  */
-function handleBetSelectClick(e) {
-  if (e.button === 'select')
-    setBetAmount();
+function launchAmountSelectionMenu(selectedBet) {
+  var amnountsMenuItems = createAmountMenuItems(selectedBet);
+  curMenu = new Menu(selectedBet.label, 'Select amount', amnountsMenuItems, launchHandshakePrompt);
+  curMenu.render();
 }
 
-function handleBetAmountClick(e) {
-  if (e.button === 'select') {
-    promptHandshake();
-  } else {
-    var newAmountIndex = curBet.amountIndex;
-    if (e.button === 'up') {
-      newAmountIndex++;
-    } else if (e.button === 'down') {
-      newAmountIndex--;
-    }
-    if (newAmountIndex >= betAmounts.length) newAmountIndex = 0;
-    if (newAmountIndex < 0) newAmountIndex = betAmounts.length - 1;
-
-    // simply.body(totalButtonClicks + ', e.button: ' + e.button + '. setting amount to: ' + amount);
-    curBet.amountIndex = newAmountIndex;
-    curBet.amount      = betAmounts[newAmountIndex];
-    renderBetAmount();
-  }
+function launchHandshakePrompt(selectedAmount) {
+  curMode = Modes.WAIT_FOR_HAND_SHAKE;
+  curBet = createBet(selectedAmount);
+  renderHandshakePrompt(curBet.betType.label, curBet.amountType.label);
 }
+
+// function handleBetSelectClick(e) {
+//   if (e.button === 'select')
+//     setBetAmount();
+// }
+
+// function handleBetAmountClick(e) {
+//   if (e.button === 'select') {
+//     promptHandshake();
+//   } else {
+//     var newAmountIndex = curBet.amountIndex;
+//     if (e.button === 'up') {
+//       newAmountIndex++;
+//     } else if (e.button === 'down') {
+//       newAmountIndex--;
+//     }
+//     if (newAmountIndex >= BET_AMOUNTS.length) newAmountIndex = 0;
+//     if (newAmountIndex < 0) newAmountIndex = BET_AMOUNTS.length - 1;
+
+//     // simply.body(totalButtonClicks + ', e.button: ' + e.button + '. setting amount to: ' + amount);
+//     curBet.amountIndex = newAmountIndex;
+//     curBet.amount      = BET_AMOUNTS[newAmountIndex];
+//     renderBetAmount();
+//   }
+// }
 
 simply.on('singleClick', function(e) {
   if (curMode === Modes.MENU)
     curMenu.handleClick(e);
-  else if (curMode === Modes.SELECT_BET)
-    handleBetSelectClick(e);
-  else if (curMode === Modes.SET_BET_AMOUNT)
-    handleBetAmountClick(e);
+  // else if (curMode === Modes.SELECT_BET)
+  //   handleBetSelectClick(e);
+  // else if (curMode === Modes.SET_BET_AMOUNT)
+  //   handleBetAmountClick(e);
 });
 
 // respond to handshake event
 simply.on('accelTap', function(e) {
-  if (curMode == Modes.WAIT_FOR_HAND_SHAKE)
+  if (curMode === Modes.WAIT_FOR_HAND_SHAKE)
     sendBet();
 });
 
 /*
  * Logic functions
  */
-function setBetAmount() {
-  curMode = Modes.SET_BET_AMOUNT;
-  renderBetAmount();
-}
-
-function promptHandshake() {
-  curMode = Modes.WAIT_FOR_HAND_SHAKE;
-  renderHandshakePrompt();
-}
-
 function sendBet() {
   var id = Pebble.getAccountToken();
   ajax({
@@ -217,11 +256,4 @@ function waitForConfirmation() {
   renderWaitScreen();
 }
 
-// start();
-
-clearScreen();
-curMode = Modes.MENU;
-curMenu = new Menu('Select a bet', BET_TYPES, function() {
-  console.log('I was clicked');
-});
-curMenu.render();
+start();
