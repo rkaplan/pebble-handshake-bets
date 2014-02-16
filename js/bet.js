72f9a23c850a257409c22937b6a91f5620718375
Russell Kaplan <russell.j.kaplan@gmail.com>
2014-02-15 19:18:00 -0500

    much cleanup, so wow

--- js/bet.js
+++ js/bet.js
@@ -2,70 +2,127 @@ var betAmounts = [1, 5, 10, 20, 50, 100];
 var Modes = {
   'SELECT_BET': 0,
   'SET_BET_AMOUNT': 1,
-  'WAIT_FOR_HAND_SHAKE': 2
+  'WAIT_FOR_HAND_SHAKE': 2,
+  'WAIT_FOR_CONFIRMATION': 3,
+  'BET_CONFIRMED': 4
 };
 var curMode = Modes.SELECT_BET;
-var totalButtonClicks = 0;
+var curBet = {
+  'amountIndex': 0,
+  'amount': betAmounts[0],
+  'betType': 'Test'
+};
 
-function renderBetSelection() {
-  simply.title('Select a bet:');
-  simply.subtitle('(click to select example)');
+/*
+ * Rendering functions
+ */
+function clearScreen() {
+  simply.title('');
+  simply.subtitle('');
   simply.body('');
 }
 
-function renderBetAmount(amount) {
+function renderBetSelection() {
+  clearScreen();
+  simply.title('Select a bet:');
+  simply.subtitle('(click to select example)');
+}
+
+function renderBetAmount() {
+  clearScreen();
   simply.title('Set bet amount:');
-  simply.subtitle('$' + betAmounts[amount] + '.00');
+  simply.subtitle('$' + curBet.amount + '.00');
 }
 
-function setBetAmount() {
-  curMode = Modes.SET_BET_AMOUNT;
-  var startAmount = 0;
-  localStorage.setItem('betAmount', startAmount);
-  renderBetAmount(startAmount);
+function renderHandshakePrompt() {
+  clearScreen();
+  simply.title('Shake hands to bet $' + curBet.amount + '.00 on ' + curBet.betType);
 }
 
+function renderWaitScreen() {
+  clearScreen();
+  simply.title('Processing...');
+}
+
+function renderBetConfirmed() {
+  clearScreen();
+  simply.title('Bet confirmed!');
+}
+
+/*
+ * Handler functions
+ */
 function handleBetSelectClick(e) {
   if (e.button === 'select')
     setBetAmount();
 }
 
 function handleBetAmountClick(e) {
-  var amount = localStorage.getItem('betAmount') || 0;
-  if (e.button === 'up') {
-    amount++;
-  } else if (e.button === 'down') {
-    amount--;
-  }
-  if (amount >= betAmounts.length) amount = 0;
-  if (amount < 0) amount = betAmounts.length - 1;
+  if (e.button === 'select') {
+    promptHandshake();
+  } else {
+    var newAmountIndex = curBet.amountIndex;
+    if (e.button === 'up') {
+      newAmountIndex++;
+    } else if (e.button === 'down') {
+      newAmountIndex--;
+    }
+    if (newAmountIndex >= betAmounts.length) newAmountIndex = 0;
+    if (newAmountIndex < 0) newAmountIndex = betAmounts.length - 1;
 
-  simply.body(totalButtonClicks + ', e.button: ' + e.button + '. setting amount from ' + oldAmount + ' to: ' + amount);
-  localStorage.setItem('betAmount', amount);
-  renderBetAmount(amount);
+    // simply.body(totalButtonClicks + ', e.button: ' + e.button + '. setting amount to: ' + amount);
+    curBet.amountIndex = newAmountIndex;
+    curBet.amount      = betAmounts[newAmountIndex];
+    renderBetAmount();
+  }
 }
 
 simply.on('singleClick', function(e) {
-  console.log("SingleClick called: " + JSON.stringify(e));
-  totalButtonClicks++;
   if (curMode === Modes.SELECT_BET)
     handleBetSelectClick(e);
   else if (curMode === Modes.SET_BET_AMOUNT)
     handleBetAmountClick(e);
 });
 
+// respond to handshake event
 simply.on('accelTap', function(e) {
-  if (curMode == Modes.WAIT_FOR_HAND_SHAKE) {
-    var id = Pebble.getAccountToken();
-    ajax({
-          url: 'http://betsonapp.com/shake?challenger_token='+id,
-          method: 'POST'
-      }, function(data){
-          // var headline = data.match(/<h1>(.*?)<\/h1>/)[1];
-          simply.title("Got reply");
-          simply.vibe('short');
-      });
-  }
+  if (curMode == Modes.WAIT_FOR_HAND_SHAKE)
+    sendBet();
 });
 
+/*
+ * Logic functions
+ */
+function setBetAmount() {
+  curMode = Modes.SET_BET_AMOUNT;
+  renderBetAmount();
+}
+
+function promptHandshake() {
+  curMode = Modes.WAIT_FOR_HAND_SHAKE;
+  renderHandshakePrompt();
+}
+
+function sendBet() {
+  var id = Pebble.getAccountToken();
+  ajax({
+        url: 'http://betsonapp.com/shake',
+        method: 'POST',
+        data: {'pebble_token': id}
+    }, receiveBetConfirmation);
+  waitForConfirmation();
+}
+
+function receiveBetConfirmation(data) {
+  curMode = Modes.BET_CONFIRMED;
+  simply.vibe('short');
+  renderBetConfirmed();
+}
+
+function waitForConfirmation() {
+  curMode = Modes.WAIT_FOR_CONFIRMATION;
+  renderWaitScreen();
+}
+
+clearScreen();
 renderBetSelection();
